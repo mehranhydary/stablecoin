91c9781b3bd8d6a90f4f63d9516b62c972f0b5f7
mehranhydary <mehranhydary@users.noreply.github.com>
2023-06-05 20:12:20 -0400

    Template added

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Making a stablecoin that is:
 
 1. The relative stability will be anchored or pegged to the US dollar
 2. The stability mechanism (the way we do minting) will be algorithmic (i.e. decentralized)
+3. Collateral type for this stablecoin will be exogenous (crypto); can use a Milady NFTx vault positions
---
+++ src/Stablecoin.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+
+// Layout of contract
+// Version, imports, errors, interfaces, libraries, contracts, type declarations,
+// state variables, events, modifiers, functions
+
+// Layout of functions
+// constructor, receive, fallback, external, public, internal,
+// privaate, view, pure functions
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title Stablecoin
+ * @author 𝖒
+ * Collateral: Milady NFTx Vault
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ *
+ * This is the contract governed by MiladySCEngine. This contract is just the
+ * ERC20 implementation of the stablecoin system.
+ */
+contract MiladyStablecoin {
+    constructor() {}
+}
